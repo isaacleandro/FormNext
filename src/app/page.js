f667bc10196905932272cd1d0a6ead0d399ac91b
Isaac Leandro Luna Araujo <isaac6126@DIGITALCOLLEGE.LOCAL>
2024-07-01 21:34:40 -0300

    adicionado os inputs

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
+
+"use client"
 import Image from "next/image";
+import { Eye } from "@phosphor-icons/react";
 
 export default function Home() {
   return (
@@ -19,15 +22,42 @@ export default function Home() {
       />
       <div className="absolute inset-0 bg-indigo-800 opacity-45" />
 
-      <div className="flex min-w-full min-h-screen z-10">
-        <div className="flex-1">
-          <h1 className="text-white font-black text-4xl py-10">
+      <div className="flex min-w-full min-h-screen z-10 py-10">
+        <div className="flex-[1.7]">
+          <h1 className="text-white font-black text-3xl ">
             Techbia
           </h1>
         </div>
-        <div className="flex-1 justify-center items-center py-10 px-12">
-          <div className="bg-white rounded-3x1 min-w-full min-h-full flex justify-center items-center ">
-            sdg
+        <div className=" flex-1 justify-center items-center px-5">
+          <div className="gap-5 bg-white rounded-3xl min-w-full min-h-full flex justify-center items-start flex-col p-8 text-black">
+            <h1 className="text-3xl font-bold text-black">
+              Let&apos;s create account.
+            </h1>
+            <p>Get stardet by creating your account</p>
+
+            <div className="flex min-w-full gap-4">
+              <input className="border-none focus:outline-none bg-opacity-30 bg-gray-400 rounded-2xl plceholder-gray-400 p-4 flex-1" placeholder="First Name" />
+
+              <input className="border-none focus:outline-none bg-opacity-30 bg-gray-400 rounded-2xl plceholder-gray-400 p-4 flex-1" placeholder="Last Name" />
+            </div>
+            <input className="border-none focus:outline-none bg-opacity-30 bg-gray-400 rounded-2xl plceholder-gray-400 p-4 min-w-full" placeholder="Email" />
+            <div className="h-16 w-full bg-gray-200 rounded-xl flex items-center px-4 gap-4">
+              <Image src="/assets/bandeira.png" alt="arrow" style={{ zIndex: 20 }} width={40} height={40} />
+              <p>+ 1</p>
+              <div className="w-[1.5px] h-[80%] bg-gray-400 rounded " />
+              <input className="flex-grow bg-transparent focus:outline-none" placeholder="Phone Number " />
+            </div>
+
+            <div className="flex gap-4">
+              <div className="flex h-16 w-full bg-gray-200 rounded-xl flex items-center px-4">
+                <input className="flex-grow bg-transparent focus:outline-none" placeholder="Password " />
+                <Eye color="#9da3af" size={20} />
+              </div>
+              <div className="flex h-16 w-full bg-gray-200 rounded-xl flex items-center px-4">
+                <input className=" flex-grow bg-transparent focus:outline-none" placeholder="Confirm password" />
+                <Eye color="#9da3af" size={20} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
